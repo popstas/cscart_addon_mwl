3e3197e92761bedfa74c86315079b7bffccabd9f
Stanislav Popov <popstas@gmail.com>
2025-08-13 18:32:49 +0500

    fixes

--- app/addons/mwl_xlsx/controllers/backend/mwl_xlsx.php
+++ app/addons/mwl_xlsx/controllers/backend/mwl_xlsx.php
@@ -5,7 +5,7 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 if ($mode === 'dev_reload_langs') {
     // импортирует var/langs/*/addons/mwl_xlsx.po в БД
-    fn_install_addon_languages('mwl_xlsx', false);
+    fn_reinstall_addon_files('mwl_xlsx');
     fn_clear_cache(); // чтобы сразу увидеть обновления
     return [CONTROLLER_STATUS_OK, 'addons.update?addon=mwl_xlsx'];
 }
