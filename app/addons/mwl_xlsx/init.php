1743ec423986076c151ea92c9cbadf18abe749d3
Stanislav Popov <popstas@gmail.com>
2025-08-13 20:43:59 +0500

    Add SEO-friendly URLs for media lists (#16)

--- app/addons/mwl_xlsx/init.php
+++ app/addons/mwl_xlsx/init.php
@@ -3,5 +3,22 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 fn_register_hooks(
     'auth_routines_post',
-    'init_user_session_data_post'
+    'init_user_session_data_post',
+    'before_dispatch'
 );
+
+function fn_mwl_xlsx_before_dispatch(&$controller, &$mode, &$action, &$dispatch_extra, &$area)
+{
+    $path = parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
+    if (!$path) {
+        return;
+    }
+
+    if (preg_match('~/(?:[a-z]{2}/)?media-lists/(\d+)/?$~i', $path, $m)) {
+        $_REQUEST['dispatch'] = 'mwl_xlsx.view';
+        $_REQUEST['list_id']  = (int) $m[1];
+
+        $controller = 'mwl_xlsx';
+        $mode       = 'view';
+    }
+}
