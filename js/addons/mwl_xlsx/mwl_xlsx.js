518fa71ffd16b1a508ef0230f4deb729ae87ef59
Stanislav Popov <popstas@gmail.com>
2025-08-13 17:39:31 +0500

    Use Tygh dialogs and icons for wishlist actions

--- js/addons/mwl_xlsx/mwl_xlsx.js
+++ js/addons/mwl_xlsx/mwl_xlsx.js
@@ -33,42 +33,73 @@
         return false;
     });
 
+    var $renameDialog = $('#mwl_xlsx_rename_dialog');
+    var $deleteDialog = $('#mwl_xlsx_delete_dialog');
+
     $(document).on('click', '[data-ca-mwl-rename]', function() {
         var $li = $(this).closest('[data-ca-mwl-list-id]');
         var list_id = $li.data('caMwlListId');
         var current_name = $li.find('[data-ca-mwl-list-name]').text();
-        var name = prompt(_.tr('mwl_xlsx.enter_list_name') || 'Enter list name', current_name);
-        if (name && name !== current_name) {
+        $renameDialog.data('caMwlListId', list_id);
+        $('#mwl_xlsx_rename_input').val(current_name);
+        $renameDialog.ceDialog('open', {
+            title: _.tr('mwl_xlsx.rename') || 'Rename'
+        });
+        return false;
+    });
+
+    $(document).on('click', '[data-ca-mwl-rename-save]', function() {
+        var list_id = $renameDialog.data('caMwlListId');
+        var name = $('#mwl_xlsx_rename_input').val().trim();
+        if (name) {
             $.ceAjax('request', fn_url('mwl_xlsx.rename_list'), {
                 method: 'post',
                 data: { list_id: list_id, name: name },
                 callback: function(data) {
                     if (data && data.success) {
-                        $li.find('[data-ca-mwl-list-name]').text(data.name);
+                        $('[data-ca-mwl-list-id="' + list_id + '"]').find('[data-ca-mwl-list-name]').text(data.name);
                     }
                 }
             });
         }
+        $renameDialog.ceDialog('close');
+        return false;
+    });
+
+    $(document).on('click', '[data-ca-mwl-rename-cancel]', function() {
+        $renameDialog.ceDialog('close');
         return false;
     });
 
     $(document).on('click', '[data-ca-mwl-delete]', function() {
-        var $li = $(this).closest('[data-ca-mwl-list-id]');
-        var list_id = $li.data('caMwlListId');
-        if (confirm(_.tr('mwl_xlsx.confirm_remove') || 'Remove this list?')) {
-            $.ceAjax('request', fn_url('mwl_xlsx.delete_list'), {
-                method: 'post',
-                data: { list_id: list_id },
-                callback: function(data) {
-                    if (data && data.success) {
-                        $li.remove();
-                        if (!$('[data-ca-mwl-list-id]').length) {
-                            location.reload();
-                        }
+        var list_id = $(this).closest('[data-ca-mwl-list-id]').data('caMwlListId');
+        $deleteDialog.data('caMwlListId', list_id);
+        $deleteDialog.ceDialog('open', {
+            title: _.tr('mwl_xlsx.remove') || 'Remove'
+        });
+        return false;
+    });
+
+    $(document).on('click', '[data-ca-mwl-delete-confirm]', function() {
+        var list_id = $deleteDialog.data('caMwlListId');
+        $.ceAjax('request', fn_url('mwl_xlsx.delete_list'), {
+            method: 'post',
+            data: { list_id: list_id },
+            callback: function(data) {
+                if (data && data.success) {
+                    $('[data-ca-mwl-list-id="' + list_id + '"]').remove();
+                    if (!$('[data-ca-mwl-list-id]').length) {
+                        location.reload();
                     }
                 }
-            });
-        }
+            }
+        });
+        $deleteDialog.ceDialog('close');
+        return false;
+    });
+
+    $(document).on('click', '[data-ca-mwl-delete-cancel]', function() {
+        $deleteDialog.ceDialog('close');
         return false;
     });
 
--- var/themes_repository/abt__unitheme2/templates/addons/mwl_xlsx/views/mwl_xlsx/manage.tpl
+++ var/themes_repository/abt__unitheme2/templates/addons/mwl_xlsx/views/mwl_xlsx/manage.tpl
@@ -6,8 +6,8 @@
                 <a data-ca-mwl-list-name href="{fn_url("mwl_xlsx.list?list_id=`$l.list_id`")}">{$l.name}</a>
                 ({$l.products_count})
                 <a href="{fn_url("mwl_xlsx.export?list_id=`$l.list_id`")}">{__("mwl_xlsx.export")}</a>
-                <a href="#" data-ca-mwl-rename>{__("mwl_xlsx.rename")}</a>
-                <a href="#" data-ca-mwl-delete>{__("mwl_xlsx.remove")}</a>
+                <a href="#" data-ca-mwl-rename title="{__("mwl_xlsx.rename")}"><i class="ut2-icon-baseline-edit"></i></a>
+                <a href="#" data-ca-mwl-delete title="{__("mwl_xlsx.remove")}"><i class="ut2-icon-baseline-delete"></i></a>
             </li>
         {/foreach}
         </ul>
@@ -15,6 +15,25 @@
         {include file="common/no_items.tpl"}
     {/if}
 {/capture}
+
+<div id="mwl_xlsx_rename_dialog" class="hidden">
+    <div class="ty-control-group">
+        <label for="mwl_xlsx_rename_input" class="ty-control-group__title">{__("mwl_xlsx.enter_list_name")}</label>
+        <input type="text" id="mwl_xlsx_rename_input" class="ty-input-text" />
+    </div>
+    <div class="buttons-container">
+        <button class="ty-btn ty-btn__primary" data-ca-mwl-rename-save>{__("save")}</button>
+        <button class="ty-btn" data-ca-mwl-rename-cancel>{__("cancel")}</button>
+    </div>
+</div>
+
+<div id="mwl_xlsx_delete_dialog" class="hidden">
+    <div class="ty-dialog-body">{__("mwl_xlsx.confirm_remove")}</div>
+    <div class="buttons-container">
+        <button class="ty-btn ty-btn__primary" data-ca-mwl-delete-confirm>{__("mwl_xlsx.remove")}</button>
+        <button class="ty-btn" data-ca-mwl-delete-cancel>{__("cancel")}</button>
+    </div>
+</div>
 
 {include file="blocks/wrappers/mainbox_simple.tpl"
     title=__("mwl_xlsx.my_lists")
