514a5dc934da5f33c0e536c61f316f5f98d7f683
Stanislav Popov <popstas@gmail.com>
2025-09-08 16:45:33 +0500

    Update currencies from json

--- app/addons/mwl_xlsx/controllers/backend/mwl_xlsx.php
+++ app/addons/mwl_xlsx/controllers/backend/mwl_xlsx.php
@@ -33,3 +33,32 @@ if ($mode === 'settings') {
     \Tygh::$app['view']->assign('mwl_xlsx', $settings);
     \Tygh::$app['view']->assign('usergroups', fn_get_usergroups(['type' => 'C'], CART_LANGUAGE));
 }
+
+if ($mode === 'update_currencies') {
+    $path = Registry::get('addons.mwl_xlsx.currencies_path');
+    if (empty($path) || !file_exists($path)) {
+        return [CONTROLLER_STATUS_NO_CONTENT];
+    }
+
+    $data = json_decode(file_get_contents($path), true);
+    if (!is_array($data)) {
+        return [CONTROLLER_STATUS_NO_CONTENT];
+    }
+
+    $symbols = db_get_hash_array('SELECT currency_code, symbol FROM ?:currencies', 'currency_code');
+
+    foreach ($data as $code => $coef) {
+        $code = strtoupper($code);
+        $currency_id = db_get_field('SELECT currency_id FROM ?:currencies WHERE currency_code = ?s', $code);
+        if ($currency_id) {
+            $currency_data = [
+                'currency_code' => $code,
+                'symbol' => $symbols[$code]['symbol'],
+                'coefficient'   => $coef,
+            ];
+            fn_update_currency($currency_data, $currency_id);
+        }
+    }
+
+    return [CONTROLLER_STATUS_NO_CONTENT];
+}
