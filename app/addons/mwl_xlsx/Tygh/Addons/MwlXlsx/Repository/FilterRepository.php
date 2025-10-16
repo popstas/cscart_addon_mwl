c42a39e172a5fef686ef33c5b46bb7cd7fa6a216
Stanislav Popov <popstas@gmail.com>
2025-10-16 21:03:15 +0500

    Leverage core helper to load filters

--- app/addons/mwl_xlsx/Tygh/Addons/MwlXlsx/Repository/FilterRepository.php
+++ app/addons/mwl_xlsx/Tygh/Addons/MwlXlsx/Repository/FilterRepository.php
@@ -21,32 +21,72 @@ class FilterRepository
      */
     public function getCompanyFilters(int $company_id): array
     {
-        $filters = [];
-        $rows = $this->db->getArray(
-            'SELECT * FROM ?:product_filters WHERE company_id = ?i AND (field_type = ?s OR feature_id > 0)',
+        $filter_ids = $this->db->getColumn(
+            'SELECT filter_id FROM ?:product_filters WHERE company_id = ?i AND (field_type = ?s OR feature_id > 0)',
             $company_id,
             'P'
         );
 
-        if (!$rows) {
+        if (!$filter_ids) {
             return [];
         }
 
-        foreach ($rows as $row) {
-            $filter_id = (int) $row['filter_id'];
+        $filter_ids = array_filter(array_map('intval', $filter_ids));
 
-            if (!$filter_id) {
+        if (!$filter_ids) {
+            return [];
+        }
+
+        $filters = fn_product_filters_get_filters([
+            'filter_item_ids' => implode(',', $filter_ids),
+        ], \DESCR_SL);
+
+        $raw_filters = $this->db->getArray(
+            'SELECT pf.*, pfd.filter FROM ?:product_filters AS pf'
+            . ' LEFT JOIN ?:product_filter_descriptions AS pfd ON pfd.filter_id = pf.filter_id AND pfd.lang_code = ?s'
+            . ' WHERE pf.filter_id IN (?n)',
+            \DESCR_SL,
+            $filter_ids
+        );
+
+        $raw_by_id = [];
+
+        foreach ($raw_filters as $row) {
+            $filter_id = (int) ($row['filter_id'] ?? 0);
+
+            if ($filter_id <= 0) {
                 continue;
             }
 
-            $filters[$filter_id] = $row;
+            if (isset($row['params']) && is_string($row['params']) && $row['params'] !== '') {
+                $params = @unserialize($row['params'], ['allowed_classes' => false]);
+
+                if (is_array($params)) {
+                    $row['params'] = $params;
+                } else {
+                    unset($row['params']);
+                }
+            }
+
+            $raw_by_id[$filter_id] = $row;
         }
 
-        if (!$filters) {
-            return [];
+        $result = [];
+
+        foreach ($filter_ids as $filter_id) {
+            $filter_id = (int) $filter_id;
+
+            if ($filter_id <= 0) {
+                continue;
+            }
+
+            $raw_data = $raw_by_id[$filter_id] ?? [];
+            $core_data = $filters[$filter_id] ?? [];
+
+            $result[$filter_id] = array_merge($raw_data, $core_data);
         }
 
-        return $filters;
+        return $result;
     }
 
     /**
