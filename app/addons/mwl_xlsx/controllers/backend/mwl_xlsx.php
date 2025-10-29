cdd60bee92ee3c11c23971e6ec31cb1c20f40f42
Stanislav Popov <popstas@gmail.com>
2025-10-29 18:06:21 +0500

    Add CLI cleanup for unused products

--- app/addons/mwl_xlsx/controllers/backend/mwl_xlsx.php
+++ app/addons/mwl_xlsx/controllers/backend/mwl_xlsx.php
@@ -144,6 +144,210 @@ if ($mode === 'publish_down_missing_products') {
     return [CONTROLLER_STATUS_NO_CONTENT];
 }
 
+if ($mode === 'delete_unused_products') {
+    $critical_tables = [
+        '?:mwl_xlsx_list_products' => [
+            'columns' => ['product_id'],
+        ],
+        '?:order_details' => [
+            'columns' => ['product_id'],
+        ],
+        '?:product_reviews' => [
+            'columns' => ['product_id'],
+        ],
+        '?:product_sales' => [
+            'columns' => ['product_id'],
+        ],
+        '?:rma_return_products' => [
+            'columns' => ['product_id'],
+        ],
+        '?:wishlist_products' => [
+            'columns' => ['product_id'],
+        ],
+        '?:user_session_products' => [
+            'columns' => ['product_id'],
+        ],
+        '?:product_subscriptions' => [
+            'columns' => ['product_id'],
+        ],
+        '?:discussion_posts' => [
+            'columns' => ['object_id'],
+            'condition' => "object_type = 'P'",
+        ],
+    ];
+
+    $existing_tables = [];
+    $referenced_product_ids = [];
+    $db_prefix = (string) Registry::get('config.db_prefix');
+
+    foreach ($critical_tables as $table => $columns) {
+        $table_name = str_replace('?:', $db_prefix, $table);
+        $table_exists = (bool) db_get_field('SHOW TABLES LIKE ?l', $table_name);
+
+        if (!$table_exists) {
+            $warning_message = __('mwl_xlsx.delete_unused_products_table_missing', ['[table]' => $table_name]);
+            echo '[info] ' . $warning_message . PHP_EOL;
+            fn_mwl_xlsx_append_log('[delete_unused] ' . $warning_message);
+
+            continue;
+        }
+
+        $table_columns = (array) ($columns['columns'] ?? []);
+        $table_condition = isset($columns['condition']) && $columns['condition'] !== ''
+            ? ' AND ' . $columns['condition']
+            : '';
+
+        if (!$table_columns) {
+            continue;
+        }
+
+        $existing_tables[$table] = [
+            'columns' => $table_columns,
+            'condition' => $table_condition,
+        ];
+
+        foreach ($table_columns as $column) {
+            $rows = db_get_fields("SELECT DISTINCT $column FROM $table WHERE $column IS NOT NULL$table_condition");
+
+            foreach ($rows as $value) {
+                $product_id = (int) $value;
+
+                if ($product_id > 0) {
+                    $referenced_product_ids[$product_id] = true;
+                }
+            }
+        }
+    }
+
+    $all_product_ids = array_map('intval', db_get_fields('SELECT product_id FROM ?:products'));
+
+    if (!$all_product_ids) {
+        $message = __('mwl_xlsx.delete_unused_products_empty');
+        echo $message . PHP_EOL;
+        fn_mwl_xlsx_append_log('[delete_unused] ' . $message);
+
+        return [CONTROLLER_STATUS_NO_CONTENT];
+    }
+
+    $in_use_lookup = $referenced_product_ids;
+    $unused_product_ids = [];
+
+    foreach ($all_product_ids as $product_id) {
+        if ($product_id <= 0) {
+            continue;
+        }
+
+        if (!isset($in_use_lookup[$product_id])) {
+            $unused_product_ids[] = $product_id;
+        }
+    }
+
+    sort($unused_product_ids);
+
+    $summary_message = __('mwl_xlsx.delete_unused_products_summary', [
+        '[total]' => count($all_product_ids),
+        '[referenced]' => count($referenced_product_ids),
+        '[candidates]' => count($unused_product_ids),
+    ]);
+
+    echo $summary_message . PHP_EOL;
+    fn_mwl_xlsx_append_log('[delete_unused] ' . $summary_message);
+
+    if (!$unused_product_ids) {
+        $message = __('mwl_xlsx.delete_unused_products_none');
+        echo '[info] ' . $message . PHP_EOL;
+        fn_mwl_xlsx_append_log('[delete_unused] ' . $message);
+
+        return [CONTROLLER_STATUS_NO_CONTENT];
+    }
+
+    $deleted_products = [];
+    $skipped_products = [];
+    $errors = [];
+
+    $check_references = static function (int $product_id) use ($existing_tables): array {
+        $references = [];
+
+        foreach ($existing_tables as $table => $columns) {
+            foreach ($columns['columns'] as $column) {
+                $condition = $columns['condition'] ?? '';
+                $is_linked = (bool) db_get_field(
+                    "SELECT 1 FROM $table WHERE $column = ?i$condition LIMIT 1",
+                    $product_id
+                );
+
+                if ($is_linked) {
+                    $references[] = sprintf('%s.%s', str_replace('?:', '', $table), $column);
+                    break;
+                }
+            }
+        }
+
+        return $references;
+    };
+
+    foreach ($unused_product_ids as $product_id) {
+        $references = $check_references($product_id);
+
+        if ($references) {
+            $reference_message = __('mwl_xlsx.delete_unused_products_skip', [
+                '[product_id]' => $product_id,
+                '[sources]' => implode(', ', $references),
+            ]);
+
+            echo '[skip] ' . $reference_message . PHP_EOL;
+            fn_mwl_xlsx_append_log('[delete_unused] ' . $reference_message);
+
+            $skipped_products[] = $product_id;
+
+            continue;
+        }
+
+        $deleted = fn_delete_product($product_id);
+
+        if ($deleted) {
+            db_query('DELETE FROM ?:seo_names WHERE object_type = ?s AND object_id = ?i', 'p', $product_id);
+
+            $deleted_message = __('mwl_xlsx.delete_unused_products_deleted', ['[product_id]' => $product_id]);
+            echo '[deleted] ' . $deleted_message . PHP_EOL;
+            fn_mwl_xlsx_append_log('[delete_unused] ' . $deleted_message);
+
+            $deleted_products[] = $product_id;
+
+            continue;
+        }
+
+        $error_message = __('mwl_xlsx.delete_unused_products_error', ['[product_id]' => $product_id]);
+        echo '[error] ' . $error_message . PHP_EOL;
+        fn_mwl_xlsx_append_log('[delete_unused] ' . $error_message);
+
+        $errors[] = $product_id;
+    }
+
+    $result_message = __('mwl_xlsx.delete_unused_products_result', [
+        '[deleted]' => count($deleted_products),
+        '[skipped]' => count($skipped_products),
+        '[errors]' => count($errors),
+    ]);
+
+    echo $result_message . PHP_EOL;
+    fn_mwl_xlsx_append_log('[delete_unused] ' . $result_message);
+
+    if ($deleted_products) {
+        $ids_message = __('mwl_xlsx.delete_unused_products_deleted_list', ['[ids]' => implode(', ', $deleted_products)]);
+        echo '[info] ' . $ids_message . PHP_EOL;
+        fn_mwl_xlsx_append_log('[delete_unused] ' . $ids_message);
+    }
+
+    if ($errors) {
+        $errors_message = __('mwl_xlsx.delete_unused_products_errors_list', ['[ids]' => implode(', ', $errors)]);
+        echo '[error] ' . $errors_message . PHP_EOL;
+        fn_mwl_xlsx_append_log('[delete_unused] ' . $errors_message);
+    }
+
+    return [CONTROLLER_STATUS_NO_CONTENT];
+}
+
 if ($mode === 'dev_reload_langs') {
     // импортирует var/langs/*/addons/mwl_xlsx.po в БД
     fn_reinstall_addon_files('mwl_xlsx');
